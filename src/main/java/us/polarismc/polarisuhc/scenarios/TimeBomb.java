135d17374d197cc0bd0989616bbee0d9947c84f0
Diego <putindeergaming@gmail.com>
2026-05-10 19:54:20 -0400

    removed deprecated api + unnecessary isenabled check refactored scenario name

--- src/main/java/us/polarismc/polarisuhc/scenarios/TimeBomb.java
+++ src/main/java/us/polarismc/polarisuhc/scenarios/TimeBomb.java
@@ -1,10 +1,8 @@
 package us.polarismc.polarisuhc.scenarios;
 
-import net.kyori.adventure.text.Component;
-import net.kyori.adventure.text.format.NamedTextColor;
-import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.block.Chest;
 import org.bukkit.entity.ArmorStand;
 import org.bukkit.entity.EntityType;
 import org.bukkit.event.EventHandler;
@@ -17,7 +15,7 @@ import java.util.concurrent.ConcurrentMap;
 
 @Scenario(name = "TimeBomb", author = "volcqnn", icon = Material.TNT,
         description = "When a player dies, a TNT chest appears at their death location. After 30 seconds, it explodes.")
-public class TimeBombScenario extends BaseScenario {
+public class TimeBomb extends BaseScenario {
     private static final int COUNTDOWN_SECONDS = 30;
     private static final float EXPLOSION_POWER = 4.0f;
 
@@ -31,8 +29,8 @@ public class TimeBombScenario extends BaseScenario {
 
         deathLoc.getBlock().setType(Material.CHEST);
         if (event.getEntity().getKiller() != null) {
-            org.bukkit.block.Chest chest = (org.bukkit.block.Chest) deathLoc.getBlock().getState();
-            chest.setCustomName("Time Bomb");
+            Chest chest = (Chest) deathLoc.getBlock().getState();
+            chest.customName(plugin.utils.chat("Time Bomb"));
         }
 
         ArmorStand hologram = (ArmorStand) deathLoc.getWorld().spawnEntity(
@@ -41,20 +39,15 @@ public class TimeBombScenario extends BaseScenario {
         hologram.setGravity(false);
         hologram.setInvulnerable(true);
         hologram.setCustomNameVisible(true);
-        hologram.setCustomName("Explodes in " + COUNTDOWN_SECONDS + "s");
+        hologram.customName(plugin.utils.chat("<red>Explodes in " + COUNTDOWN_SECONDS + "s"));
         hologram.setMarker(true);
 
         holograms.put(deathLoc, hologram);
 
-        Bukkit.getScheduler().runTaskLater(plugin, () -> explode(deathLoc, hologram), COUNTDOWN_SECONDS * 20L);
+        plugin.utils.delay(COUNTDOWN_SECONDS * 20, () -> explode(deathLoc, hologram));
     }
 
     private void explode(Location deathLoc, ArmorStand hologram) {
-        if (!isEnabled()) {
-            cleanup(hologram);
-            return;
-        }
-
         deathLoc.getBlock().setType(Material.AIR);
 
         deathLoc.getWorld().createExplosion(deathLoc, EXPLOSION_POWER, false, false);

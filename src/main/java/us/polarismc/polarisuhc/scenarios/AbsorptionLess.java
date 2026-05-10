6c7440876624de5f042474d758bd41d839124d32
Diego <putindeergaming@gmail.com>
2026-05-10 19:51:20 -0400

    changed event to playeritemconsumeevent (more reliable) removed unneccessary isenabled checks refactored scenario name replaced runtasklater with delay from plugin.utils

--- src/main/java/us/polarismc/polarisuhc/scenarios/AbsorptionLess.java
+++ src/main/java/us/polarismc/polarisuhc/scenarios/AbsorptionLess.java
@@ -2,21 +2,22 @@ package us.polarismc.polarisuhc.scenarios;
 
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.player.PlayerItemConsumeEvent;
 import org.bukkit.potion.PotionEffectType;
 import us.polarismc.polarisuhc.managers.scenario.BaseScenario;
 import us.polarismc.polarisuhc.managers.scenario.Scenario;
 
 @Scenario(name = "AbsorptionLess", author = "volcqnn", icon = Material.GOLDEN_APPLE,
         description = "Absorption hearts are disabled, but golden apples still heal.")
-public class AbsorptionLessScenario extends BaseScenario {
-
+public class AbsorptionLess extends BaseScenario {
     @EventHandler
-    public void onEntityDamage(EntityDamageEvent event) {
-        if (!isEnabled()) return;
-        if (event.getEntity() instanceof org.bukkit.entity.Player player) {
-            Bukkit.getScheduler().runTaskLater(plugin, () -> player.removePotionEffect(PotionEffectType.ABSORPTION), 2);
+    public void onGoldenAppleConsume(PlayerItemConsumeEvent event) {
+        Material type = event.getItem().getType();
+        if (type == Material.GOLDEN_APPLE || type == Material.ENCHANTED_GOLDEN_APPLE) {
+            plugin.utils.delay(() -> event.getPlayer().removePotionEffect(PotionEffectType.ABSORPTION));
         }
     }
 }

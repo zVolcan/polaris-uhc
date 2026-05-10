6b14c354fa5f328d2d951072d2f6654dead31415
Diego <putindeergaming@gmail.com>
2026-05-10 19:53:21 -0400

    changed deserialize to actionbar method from api removed unneccessary isenabled checks refactored scenario name added copper sword

--- src/main/java/us/polarismc/polarisuhc/scenarios/SwordLess.java
+++ src/main/java/us/polarismc/polarisuhc/scenarios/SwordLess.java
@@ -1,28 +1,22 @@
 package us.polarismc.polarisuhc.scenarios;
 
-import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.minimessage.MiniMessage;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
-import org.bukkit.event.block.Action;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.inventory.CraftItemEvent;
-import org.bukkit.event.player.PlayerInteractEvent;
 import us.polarismc.polarisuhc.managers.scenario.BaseScenario;
 import us.polarismc.polarisuhc.managers.scenario.Scenario;
 
 @Scenario(name = "SwordLess", author = "volcqnn", icon = Material.DIAMOND_SWORD,
         description = "You cannot use swords.")
-public class SwordLessScenario extends BaseScenario {
-
+public class SwordLess extends BaseScenario {
     @EventHandler
     public void onPlayerInteract(EntityDamageByEntityEvent event) {
-        if (!isEnabled()) return;
-
         if (event.getDamager() instanceof Player player && isSword(player.getInventory().getItemInMainHand().getType())) {
             event.setCancelled(true);
-            player.sendActionBar(MiniMessage.miniMessage().deserialize("<red>Swords are disabled!"));
+            plugin.utils.actionBar(player, "<red>Swords are disabled!");
         }
     }
 
@@ -35,10 +29,11 @@ public class SwordLessScenario extends BaseScenario {
 
     private boolean isSword(Material type) {
         return type == Material.WOODEN_SWORD ||
-               type == Material.STONE_SWORD ||
-               type == Material.IRON_SWORD ||
-               type == Material.GOLDEN_SWORD ||
-               type == Material.DIAMOND_SWORD ||
-               type == Material.NETHERITE_SWORD;
+                type == Material.COPPER_SWORD ||
+                type == Material.STONE_SWORD ||
+                type == Material.IRON_SWORD ||
+                type == Material.GOLDEN_SWORD ||
+                type == Material.DIAMOND_SWORD ||
+                type == Material.NETHERITE_SWORD;
     }
 }

5fb6a7cd39dcefc9854ffae1055c937e4c623bf5
volcqnn <emir49499141@gmail.com>
2026-05-17 12:15:45 -0300

    fix(scenarios): update author for DoubleOres and TripleOres scenarios

--- src/main/java/us/polarismc/polarisuhc/scenarios/DoubleOres.java
+++ src/main/java/us/polarismc/polarisuhc/scenarios/DoubleOres.java
@@ -4,7 +4,7 @@ import org.bukkit.Material;
 import us.polarismc.polarisuhc.managers.scenario.Scenario;
 import us.polarismc.polarisuhc.managers.scenario.ScenarioConfig;
 
-@Scenario(name = "DoubleOres", author = "putindeer", icon = Material.RAW_IRON,
+@Scenario(name = "DoubleOres", author = "volcqnn", icon = Material.RAW_IRON,
         description = "Mining ore blocks yields double the output.",
         inDevelopment = true)
 public class DoubleOres extends CutClean {
--- src/main/java/us/polarismc/polarisuhc/scenarios/TripleOres.java
+++ src/main/java/us/polarismc/polarisuhc/scenarios/TripleOres.java
@@ -4,7 +4,7 @@ import org.bukkit.Material;
 import us.polarismc.polarisuhc.managers.scenario.Scenario;
 import us.polarismc.polarisuhc.managers.scenario.ScenarioConfig;
 
-@Scenario(name = "TripleOres", author = "putindeer", icon = Material.RAW_GOLD,
+@Scenario(name = "TripleOres", author = "volcqnn", icon = Material.RAW_GOLD,
         description = "Mining ore blocks yields triple the output.",
         inDevelopment = true)
 public class TripleOres extends CutClean {
